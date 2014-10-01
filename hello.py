418929df600149e7d189590707201046e1a7e1ed
A M Ford <fordarnold@gmail.com>
2014-10-01 14:33:10 +0300

    Tinkering with comments... Lame i know :''')

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
+# import modules to get access to many useful functions :)
+
+# Flask - a lightweight web/backend framework for Python lang
 from flask import Flask
+
 app = Flask(__name__)
 
+# create an index page route
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+# run the flask-based app when '__main__' function/method is encountered
 if __name__ == '__main__':
     app.run()
