caac02e09d4e3cc466915255f36720d9df912dd5
A M Ford <fordarnold@gmail.com>
2014-10-02 16:04:40 +0300

    Setup boilerplate code

--- README.md
+++ README.md
@@ -3,3 +3,23 @@
 This is a starting point for building web applications with the Flask python framework.
 
 You can Fork the project and add functionality to it.
+
+- Python 2.7
+- Flask 0.10
+
+### Setup instructions
+
+These instructions assume that you already have the Python language interpreter installed on your machine.
+
+- Clone / fork the project into your own local directory
+- Open terminal window
+- cd to your project directory
+        cd C:\PythonStuff\myproject
+
+- Install dependencies (found in requirements.txt) using pip
+- Run inbuilt server
+        $ python hello.py
+        * Running on http://127.0.0.1:5000/
+
+- Open http://127.0.0.1:5000/ in your browser
+- Be amazed by your shiny new Flask app
--- hello.py
+++ hello.py
@@ -1,24 +1,15 @@
 # import modules to get access to many useful functions :)
 
 # Flask - a lightweight web/backend framework for Python lang
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
+# create Flask instance
 app = Flask(__name__)
 
 # index page route
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-# index template render route
-@app.route('/html')
-def templates(name=None):
-    return render_template('index.html', name=name)
-
-# user profile route
-# @app.route('/user')
-# show_user()
+def welcome():
+    return render_template('index.html')
 
 # run the flask-based app when '__main__' function/method is encountered
 if __name__ == '__main__':
