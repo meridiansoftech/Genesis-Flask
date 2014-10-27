ca5789f9ce20cb4b40a662face135c4b51f616f0
A M Ford <fordarnold@gmail.com>
2014-10-27 13:17:29 +0300

    Rearranging of files

--- hello.py
+++ hello.py
@@ -8,8 +8,22 @@ app = Flask(__name__)
 
 # index page route
 @app.route('/')
+@app.route('/index')
+@app.route('/home')
 def welcome():
     return render_template('index.html')
+
+@app.route('/todo')
+def todo_app():
+    return render_template('task-manager.html')
+
+@app.route('/player')
+def player_app():
+    return render_template('music-player.html')
+
+@app.route('/editor')
+def editor_app():
+    return render_template('code-editor.html')
 
 # run the flask-based app when '__main__' function/method is encountered
 if __name__ == '__main__':
