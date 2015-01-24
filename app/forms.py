8565577ebae824845b0208d9b333b333a0e96641
robin hood <fordarnold@gmail.com>
2015-01-25 02:19:28 +0300

    HTML layout updates for views

--- app/forms.py
+++ app/forms.py
@@ -5,3 +5,7 @@ from wtforms.validators import DataRequired
 class LoginForm(Form):
     openid = StringField('openid', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
+
+class SignupForm(Form):
+    openid = StringField('openid', validators=[DataRequired()])
+    remember_me = BooleanField('remember_me', default=False)
