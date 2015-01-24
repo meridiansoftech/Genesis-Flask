c8aa170bfca7fc3c1c325cd5d9d0640d1297a40a
robin hood <fordarnold@gmail.com>
2015-01-24 06:25:58 +0300

    Setup hassles

---
+++ fabfile.py
@@ -0,0 +1,16 @@
+from fabric.api import env, run, cd
+
+USERNAME = 'root'
+SERVER = 'genesisflask.example.org'
+APP_NAME = 'genesisflask'
+PROJECT_DIR = '/www/%s/%s' % (SERVER, APP_NAME)
+WSGI_SCRIPT = 'application.wsgi'
+
+env.hosts = ["%s@%s" % (USERNAME, SERVER)]
+
+def deploy():
+    with cd(PROJECT_DIR):
+        run('git pull')
+        run('bin source/activate')
+        run('pip install -r requirements.txt')
+        run('touch %s' % WSGI_SCRIPT)
